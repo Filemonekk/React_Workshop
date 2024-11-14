d48a053d465ece0ca66a5b99c7e45610920e42a3
KasiaH <kah.gda@wp.pl>
2024-11-14 12:52:49 +0100

    building the layout

---
+++ 06_travel_list/src/App.js
@@ -0,0 +1,36 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <h1> 🌴 Far Away 🌴</h1>
+  );
+}
+
+function Form() {
+	return (
+		<div className='add-form'>
+			<h3>What do you need for your 😍 trip?</h3>
+		</div>
+	)
+}
+
+function PackingList() {
+	return <div className='list'>LIST</div>
+}
+
+function Stats() {
+	return (
+		<footer className="stats">
+			<em>💼You have X items on your list, and you already packed Y</em>
+		</footer>
+	)
+}

3113d371040fec34a214a3222d9a880b66b7514b
KasiaH <kah.gda@wp.pl>
2024-11-14 13:21:57 +0100

    Rendering the Items List

--- 06_travel_list/src/App.js
+++ 06_travel_list/src/App.js
@@ -1,18 +1,22 @@
+const initialItems = [
+	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'Charger', quantity: 1, packed: true },
+]
+
 export default function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Form />
-      <PackingList />
-      <Stats />
-    </div>
-  );
+	return (
+		<div className='app'>
+			<Header />
+			<Form />
+			<PackingList />
+			<Stats />
+		</div>
+	)
 }
 
 function Header() {
-  return (
-    <h1> 🌴 Far Away 🌴</h1>
-  );
+	return <h1> 🌴 Far Away 🌴</h1>
 }
 
 function Form() {
@@ -24,12 +28,32 @@ function Form() {
 }
 
 function PackingList() {
-	return <div className='list'>LIST</div>
+	return (
+		<div className='list'>
+			<ul>
+				{initialItems.map(item => (
+					<Item item={item} />
+				))}
+			</ul>
+		</div>
+	)
+}
+
+function Item({ item }) {
+	return (
+		<li>
+      <span style={item.packed ? {textDecoration:'line-through'} : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+		</li>
+	)
 }
 
 function Stats() {
 	return (
-		<footer className="stats">
+		<footer className='stats'>
 			<em>💼You have X items on your list, and you already packed Y</em>
 		</footer>
 	)

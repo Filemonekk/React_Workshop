85c0b6fb96df8a81e6e801c6e863306f787bc74c
KasiaH <kah.gda@wp.pl>
2024-12-09 13:11:28 +0100

    Using an async function

--- 10_use_popcorn/src/App.js
+++ 10_use_popcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const tempMovieData = [
 	{
@@ -47,9 +47,30 @@ const tempWatchedData = [
 
 const average = arr => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)
 
+const KEY = 'bfee9e04'
+
 export default function App() {
-	const [movies, setMovies] = useState(tempMovieData)
-	const [watched, setWatched] = useState(tempWatchedData)
+	const [movies, setMovies] = useState([])
+	const [watched, setWatched] = useState([])
+	const query = 'interstellar'
+
+	//Converting promise to async function
+	//useEffect(function () {
+	//	fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+	//		.then(res => res.json())
+	//		.then(data => setMovies(data.Search));
+	//}, []);
+
+	useEffect(function () {
+		async function fetchMovies() {
+			const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
+			const data = await res.json()
+			setMovies(data.Search)
+			console.log(data.Search)
+		}
+		fetchMovies()
+	}, [])
+
 	return (
 		<>
 			<NavBar>
